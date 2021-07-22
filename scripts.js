82fdd488cc0c60e0cbd00f0c8033ffd8dd1438bf
Brett Boyd <79927828+bboyd01@users.noreply.github.com>
2021-07-22 10:35:22 -0500

    added lightgallery, added button anim

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
-$(function() {
+function swapIcon(target) {
+    var item = $(target).find("i");
+    if (item.hasClass("fa-chevron-right")) {
+        $(item).removeClass("fa-chevron-right");
+        $(item).addClass("fa-chevron-down");
+    } else {
+        $(item).removeClass("fa-chevron-down");
+        $(item).addClass("fa-chevron-right");
+    }
+}
 
+$(function() {
+    // lightgallery library
+    lightGallery(document.getElementById("lightgallery1a"), {
+        plugins: [lgThumbnail],
+        speed: 500,
+    });
+    lightGallery(document.getElementById("lightgallery1b"), {
+        plugins: [lgThumbnail],
+        speed: 500,
+    });
 
 
 
